f7aed9db655e72b7b792561021d5c102dd700244
Naspleda <chris_j18@hotmail.com>
2022-02-23 20:28:46 -0300

    Primer commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import {Task} from "./Tasks"
+
+export const TASKS: Task[] = [
+{
+    id: 1,
+    text: "Terminar primer modulo de Angular",
+    day: 'Agosto 5 a las 12:00',
+    reminder: true
+},
+
+{
+    id: 2,
+    text: "Hacer las compas para la cena",
+    day: 'Agosto 5 a las 17:00',
+    reminder: true
+},
+
+{
+    id: 3,
+    text: "Investigar sobre Bootstrap",
+    day: 'Agosto 5 a las 17:00',
+    reminder: false
+},
+
+
+{
+    id: 4,
+    text: "Leer mi libro favorito",
+    day: 'Agosto 5 a las 21:00',
+    reminder: false
+},
+
+
+]
